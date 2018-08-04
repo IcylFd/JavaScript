1b1341e34c9e0d3dd62501f0a7cd834144b6414e
lifangdi <772167928@qq.com>
2018-08-04 15:10:15 +0800

    2018.8.4 webpack

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,3 +1,4 @@
+import css from "./css/main.css";
 var a = "hello";
 var box = document.getElementById("box");
 box.innerHTML = "icy";
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const opn = require('opn');
+
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -7,9 +10,35 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback:"style-loader",
+                    use:"css-loader",
+                    publicPath:"../"
+                })
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        limit:500,
+                        outputPath: 'images/'
+                    }
+                }]
+            },
+            {
+                test: /\.(html|htm)$/i,
+                loader: 'html-withimg-loader'
+            },
 
+
+        ]
     },
     plugins:[
+        new ExtractTextPlugin("./css/main.css"),
         new HtmlWebpackPlugin({
             title:'Output Management',
             minify:{
@@ -21,5 +50,6 @@ module.exports = {
     ],
     devServer:{
 
-    }
+    },
+
 };
